8fae3557582bf9f6933bff0cf58d250406002ff1
MaxxAferist <maxim.lev.8@mail.ru>
2021-11-23 19:09:54 +0300

    Bananas))0)0))

---
+++ main.py
@@ -0,0 +1,36 @@
+from PyQt5 import uic
+import sys
+from random import randrange
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.paint)
+        self.do_paint = False
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw(self, qp):
+        x, y, r = randrange(300), randrange(200), randrange(200)
+        qp.setBrush(QColor('yellow'))
+        qp.drawEllipse(x, y, r, r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
